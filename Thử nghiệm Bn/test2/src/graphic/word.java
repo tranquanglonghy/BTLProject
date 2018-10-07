37098eec84752a164d7f50376445805df38f10cd
tranquanglonghy <37334293+tranquanglonghy@users.noreply.github.com>
2018-10-07 23:52:10 +0700

    Add files via upload

---
+++ Thử nghiệm Bn/test2/src/graphic/word.java
@@ -0,0 +1,31 @@
+package graphic;
+
+public class word {
+	
+	private String word_target;
+	private String word_explain;
+	
+	public void set_word(String word) {
+		word_target = word;
+	}
+	public void set_mean(String mean) {
+		word_explain = mean;
+	}
+	public String get_word() {
+		return this.word_target;
+	}
+	public String get_mean() {
+		return this.word_explain;
+	}
+	public word()
+	{
+		word_target = "";
+		word_explain = "";
+	}
+	public word(String word, String mean)
+	{
+		word_target = word;
+		word_explain = mean;
+	}
+        
+}
